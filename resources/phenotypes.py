30de8ee7d3a6cb191b4e3b27e83b652024b4c784
konradjk <konradjkarczewski@gmail.com>
2019-12-12 15:29:55 -0500

    adding all resource paths

--- resources/phenotypes.py
+++ resources/phenotypes.py
@@ -1,4 +1,18 @@
 
-phesant_all_phenos_tsv_paths = 'gs://ukb-diverse-pops/Phenotypes/Everyone/uk_round2_allSamples_phenos_phesant.*.tsv'
-phesant_biomarker_phenotypes_tsv_path = 'gs://ukb-diverse-pops/Phenotypes/Everyone/uk_round2_allSamples_biomarkers_phesant.tsv'
+bucket = 'gs://ukb-diverse-pops'
+pheno_folder = f'{bucket}/Phenotypes/Everyone'
+
+phesant_all_phenos_tsv_paths = f'{pheno_folder}/uk_round2_allSamples_phenos_phesant_QC.{{}}.tsv.gz'
+phesant_biomarker_phenotypes_tsv_path = f'{pheno_folder}/uk_round2_allSamples_biomarkers_phesant.tsv'
+
+pre_phesant_tsv_path = f'{pheno_folder}/neale_lab_parsed_QC_Oct2019.tsv'
+pre_phesant_biomarkers_tsv_path = f'{pheno_folder}/neale_lab_parsed_biomarkers.tsv'
+
+
+def get_ukb_pheno_ht_path(pop: str = 'all_pops', sex: str = 'both_sexes'):
+    return f'{pheno_folder}/ht/{pop}_{sex}.ht'
+
+
+def get_ukb_pheno_mt_path(data_type: str, pop: str = 'all_pops', sex: str = 'both_sexes'):
+    return f'{pheno_folder}/{sex}/{pop}_{data_type}.mt'
 
