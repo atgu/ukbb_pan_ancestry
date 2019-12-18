50b048f2912999619976070015b08181cf338f01
konradjk <konradjkarczewski@gmail.com>
2019-12-17 19:28:53 -0500

    adding pheno count by pop scripts

---
+++ phenos_by_pop.py
@@ -0,0 +1,34 @@
+from gnomad_hail import *
+from ukbb_pan_ancestry import *
+from ukb_phenotypes import *
+
+temp_bucket = 'gs://ukbb-diverse-temp-30day'
+
+
+def main():
+    data_types =  ('categorical', 'continuous', 'icd')
+    hl.init(default_reference='GRCh37', log='/pheno_agg.log')
+
+    for data_type in data_types:
+        mt = hl.read_matrix_table(get_ukb_pheno_mt_path(data_type))
+        meta_ht = hl.import_table('gs://ukb-diverse-pops/globalref_ukbb_pca_pops_rf_50.txt.bgz', impute=True, key='s')
+        mt = mt.annotate_rows(**meta_ht[mt.row_key])
+
+        if data_type == 'continuous':
+            mt = mt.filter_cols(mt.coding != 'raw')
+
+        extra_fields = compute_n_cases(mt, data_type)
+        ht = mt.group_rows_by('pop').aggregate(**extra_fields).entries()
+        ht.export(get_phenotype_summary_tsv_path(data_type))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--slack_channel', help='Send message to Slack channel/user', default='@konradjk')
+    args = parser.parse_args()
+
+    if args.slack_channel:
+        try_slack(args.slack_channel, main)
+    else:
+        main()
