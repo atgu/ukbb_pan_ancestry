b597bd8efd2c4540c27c61d0f47eb5d95c7f2151
konradjk <konradjkarczewski@gmail.com>
2019-12-17 19:28:17 -0500

    hospital record paths

--- resources/phenotypes.py
+++ resources/phenotypes.py
@@ -16,3 +16,21 @@ def get_ukb_pheno_ht_path(pop: str = 'all_pops', sex: str = 'both_sexes'):
 def get_ukb_pheno_mt_path(data_type: str, pop: str = 'all_pops', sex: str = 'both_sexes'):
     return f'{pheno_folder}/{sex}/{pop}_{data_type}.mt'
 
+
+def get_hesin_raw_data_path(data_type: str = None):
+    assert data_type in (None, 'diag', 'oper', 'delivery', 'maternity', 'psych')
+    data_type = "_" + data_type if data_type else ""
+    return f'gs://ukb31063/ukb31063.hesin{data_type}.20191008.txt'
+
+
+def get_hesin_mt_path(data_type: str):
+    assert data_type in ('diag', 'oper')
+    return f'{pheno_folder}/hesin/{data_type}.mt'
+
+
+def get_hesin_delivery_ht_path():
+    return f'{pheno_folder}/hesin/delivery.ht'
+
+
+def get_phenotype_summary_tsv_path(data_type: str):
+    return f'{pheno_folder}/summary/phenos_{data_type}.tsv'
