b3060881827c93ba89f00b8dcdc75758e3833789
konradjk <konradjkarczewski@gmail.com>
2020-04-30 14:01:00 -0400

    working load_all_results.py

--- load_all_results.py
+++ load_all_results.py
@@ -6,8 +6,9 @@ import sys
 import argparse
 from pprint import pprint
 from datetime import date
-from ukb_common import *
+from collections import defaultdict, Counter
 from ukbb_pan_ancestry import *
+from tqdm import tqdm
 
 
 def get_all_valid_variant_results_ht_paths(pop):
@@ -18,11 +19,9 @@ def get_all_valid_variant_results_ht_paths(pop):
 
 
 def get_heritability_dict(pop):
-    heritability_ht = hl.import_table(get_heritability_txt_path(), impute=True)
+    heritability_ht = hl.import_table(get_heritability_txt_path(), impute=True, key=PHENO_KEY_FIELDS)
     heritability_ht = heritability_ht.filter(heritability_ht.pop == pop)
-    heritability_dict = create_broadcast_dict(
-        hl.struct(pheno=heritability_ht.pheno, coding=heritability_ht.coding, trait_type=heritability_ht.trait),
-        hl.struct(heritability=heritability_ht.heritability, inv_normalized=heritability_ht.inv_normalized))
+    heritability_dict = create_broadcast_dict(heritability_ht.key)
     return heritability_dict
 
 
@@ -31,7 +30,7 @@ def generate_lambda_ht(pop):
     mt = mt.annotate_cols(lambda_gc=hl.methods.statgen._lambda_gc_agg(mt.Pvalue),
                           n_vars=hl.agg.count_where(hl.is_defined(mt.Pvalue)))
     ht = mt.cols()
-    ht = ht.key_by(pop=pop, pheno=ht.pheno, coding=ht.coding, trait_type=ht.trait_type)
+    ht = ht.key_by(pop=pop, *PHENO_KEY_FIELDS)
     return ht
 
 
@@ -39,13 +38,16 @@ def generate_sumstats_mt(all_variant_outputs, heritability_dict, pheno_dict, tem
     row_keys = ['locus', 'alleles', 'gene', 'annotation']
     col_keys = PHENO_KEY_FIELDS
 
-    all_hts = list(map(lambda x: unify_saige_ht_schema(hl.read_table(x), patch_case_control_count=x), all_variant_outputs))
-    mt = join_pheno_hts_to_mt(all_hts, row_keys, col_keys, pheno_dict, temp_dir,
+    all_hts = [unify_saige_ht_schema(hl.read_table(x), patch_case_control_count=x) for x in tqdm(all_variant_outputs)]
+    mt = join_pheno_hts_to_mt(all_hts, row_keys, col_keys, temp_dir=temp_dir,
                               inner_mode=inner_mode, repartition_final=20000)
-    entry = heritability_dict.get(mt.col_key)
-    mt = mt.annotate_cols(saige_heritability=entry.heritability)  #, saige_inv_normalized=entry.inv_normalized)
-    mt = mt.filter_cols(mt.pheno != "").drop('varT', 'varTstar', 'Is.SPA.converge',
-                                             'AC_Allele2', 'N', 'Tstat')
+    # Patch for accidental coding of irnt-whr as "whr" for SAIGE run
+    mt = mt.key_cols_by(**{x: hl.if_else((mt.phenocode == 'whr') & (x == 'modifier'), 'irnt', mt[x])
+                           for x in PHENO_KEY_FIELDS})
+    key = mt.col_key.annotate(phenocode=format_pheno_dir(mt.phenocode))
+    mt = mt.annotate_cols(**pheno_dict.get(key), **heritability_dict.get(key))
+    mt = mt.filter_cols(mt.phenocode != "").drop('varT', 'varTstar', 'Is.SPA.converge',
+                                                 'AC_Allele2', 'N', 'Tstat')
     mt = mt.key_rows_by('locus', 'alleles')
     return mt
 
@@ -62,11 +64,11 @@ def write_full_mt(overwrite):
     for mt in mts[1:]:
         full_mt = full_mt.union_cols(mt, row_join_type='outer')
     full_mt = full_mt.collect_cols_by_key()
+    description_fields = ('description', 'description_more', 'coding_description', 'category')
     full_mt = full_mt.annotate_cols(
-        pheno_data=full_mt.pheno_data.map(lambda x: x.drop('data_type', 'meaning', 'path',
+        pheno_data=full_mt.pheno_data.map(lambda x: x.drop(*description_fields,
                                                            'n_cases_both_sexes', 'n_cases_males', 'n_cases_females')),
-        meaning=full_mt.pheno_data.meaning[0],
-        description=full_mt.pheno_data.path[0],
+        **{x: full_mt.pheno_data[x][0] for x in description_fields},
         **{f'n_cases_full_cohort_{sex}': full_mt.pheno_data[f'n_cases_{sex}'][0]
            for sex in ('both_sexes', 'females', 'males')}
     )
@@ -85,9 +87,9 @@ def generate_and_write_lambdas(overwrite):
     pheno_ht = hl.read_table(get_phenotype_summary_path('full')).select('n_cases_by_pop', 'stats')
     full_ht = full_ht.annotate(**pheno_ht[full_ht.key])
     full_ht = full_ht.checkpoint(f'{bucket}/combined_results/all_lambdas.ht', overwrite=overwrite, _read_if_exists=not overwrite)
-    full_ht.group_by('pop').aggregate(lambda_gc=hl.agg.stats(full_ht.lambda_gc).drop('sum')).show(width=200)
-    full_ht.group_by('pheno', 'meaning').aggregate(lambda_gc=hl.agg.stats(full_ht.lambda_gc).drop('sum')).show(20, width=200)
-    full_ht.group_by('pop').aggregate(n_samples=hl.agg.max(full_ht.n_cases_by_pop), n_phenos=hl.agg.count()).show()
+    # full_ht.group_by('pop').aggregate(lambda_gc=hl.agg.stats(full_ht.lambda_gc).drop('sum')).show(width=200)
+    # full_ht.group_by('pheno', 'meaning').aggregate(lambda_gc=hl.agg.stats(full_ht.lambda_gc).drop('sum')).show(20, width=200)
+    # full_ht.group_by('pop').aggregate(n_samples=hl.agg.max(full_ht.n_cases_by_pop), n_phenos=hl.agg.count()).show()
     print('Pops per pheno:')
     pprint(dict(Counter(full_ht.aggregate(hl.agg.group_by(full_ht.key.drop('pop'), hl.agg.count()).values()))))
     full_ht.flatten().export(f'{bucket}/combined_results/all_lambdas.txt.bgz')
@@ -126,7 +128,7 @@ def main(args):
             if args.load_only:
                 pheno_matches = set(args.load_only.split(','))
                 if '' in pheno_matches:
-                    logger.warning('Empty string in pheno_matches. Might reload more than expected')
+                    print('WARNING: Empty string in pheno_matches. Might reload more than expected')
                 all_variant_outputs = [x for x in all_variant_outputs if _matches_any_pheno(x, pheno_matches)]
 
             print(f'Loading {len(all_variant_outputs)} additional HTs...')
@@ -144,7 +146,7 @@ def main(args):
             mt.write(get_variant_results_path(pop, 'mt'), overwrite=args.overwrite)
 
 
-    if args.run_basic_load or args.run_additional_load:
+    if args.run_basic_load or args.run_additional_load or args.run_combine_load:
         generate_and_write_lambdas(args.overwrite)
         write_full_mt(args.overwrite)
 
@@ -170,7 +172,7 @@ def main(args):
 
 def get_pheno_dict():
     pheno_ht = hl.read_matrix_table(get_ukb_pheno_mt_path()).cols()
-    pheno_dict = create_broadcast_dict(pheno_ht.key, pheno_ht.row_value)
+    pheno_dict = create_broadcast_dict(pheno_ht.key)
     return pheno_dict
 
 
@@ -180,6 +182,7 @@ if __name__ == '__main__':
     parser.add_argument('--overwrite', help='Overwrite everything', action='store_true')
     parser.add_argument('--run_basic_load', help='Overwrite everything', action='store_true')
     parser.add_argument('--run_additional_load', help='Overwrite everything', action='store_true')
+    parser.add_argument('--run_combine_load', help='Overwrite everything', action='store_true')
     parser.add_argument('--dry_run', help='Overwrite everything', action='store_true')
     parser.add_argument('--load_only', help='Comma-separated list of trait_type-pheno-coding to run'
                                             '(e.g. continuous-50-irnt,icd_all-E10-icd10 )')
@@ -188,6 +191,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.slack_channel:
-        try_slack(args.slack_channel, main, args)
-    else:
-        main(args)
+        from gnomad_hail.slack_creds import slack_token
+        with slack.slack_notifications(slack_token, args.slack_channel):
+            main(args)
