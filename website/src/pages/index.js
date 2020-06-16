f27c76f8aa7c41ea4aa4e5c990b4de79946d8f34
Masahiro Kanai <mkanai@broadinstitute.org>
2020-06-16 10:03:58 -0400

    fixed dark logo

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -17,7 +17,7 @@ function Home() {
       <header className={classnames('hero', styles.heroBanner)}>
         <div className="container">
           <div className="text--center">
-            <img className={styles.logo} src="img/pan_ukb_logo_v0.4-2.svg" alt={'pan ukbb title'} />
+            <img className={styles.logo} id="pan_ukbb_logo" src="img/pan_ukbb_logo_v0.4-2.svg" alt={'pan ukbb title'} />
           </div>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
