b9d58497f28bf30fc6500d18d0e19932056fe56c
Masahiro Kanai <mkanai@broadinstitute.org>
2021-02-08 20:16:27 -0500

    move ldscores to the public bucket

--- resources/ld.py
+++ resources/ld.py
@@ -6,13 +6,13 @@ def get_ld_matrix_path(pop: str, extension: str = 'bm'):
 
 
 def get_ld_score_ht_path(pop: str):
-    return f'{bucket}/ld/{pop}/UKBB.{pop}.ldscore.ht'
+    return f'{public_bucket}/ld_release/UKBB.{pop}.ldscore.ht'
 
 
 def get_ld_score_flat_file_path(pop: str, extension: str = 'ldscore.gz', rsid: bool = False):
     if extension == 'ldscore.gz' and rsid:
-        return f'{bucket}/ld/{pop}/UKBB.{pop}.rsid.l2.{extension}'
-    return f'{bucket}/ld/{pop}/UKBB.{pop}.l2.{extension}'
+        return f'{public_bucket}/ld_release/UKBB.{pop}.rsid.l2.{extension}'
+    return f'{public_bucket}/ld_release/UKBB.{pop}.l2.{extension}'
 
 
 def get_ld_variant_index_path(pop: str, extension: str = 'ht'):
